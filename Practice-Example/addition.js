a0e60090d1a7d4126feed9f7e8690ce379ddd889
Kyla <112965597+Kyla-Ren@users.noreply.github.com>
2022-09-09 19:38:02 -0400

    Add files via upload

---
+++ Practice-Example/addition.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+let c = "text";
+
+
+function runCodeToTerminal(){
+    console.log(" the value of a + b is " , a+b);
+}
+
+runCodeToTerminal();
---
+++ Practice-Example/mobile.js
@@ -0,0 +1,8 @@
+class Display{
+    show() {
+        console.log("hello world");
+    }
+}function javaT() {
+    console.log("Welcometo comp 229");
+
+}export {Display, javaT}
